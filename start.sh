2bc8b170f0e329c3740f55ed878ebcfb6268e618
Fabrizio Balliano <fabrizio@fabrizioballiano.com>
2015-12-30 22:58:27 +0000

    try again

--- start.sh
+++ start.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+set -e
 
 terminate () {
-  echo "ciaociao"
+  echo "terminating"
   /usr/local/bin/php /unregister-host-on-redis.php
   killall apache2
   exit 0
 }
 
 trap terminate HUP INT QUIT KILL TERM
-
 /usr/local/bin/php /register-host-on-redis.php
-/usr/local/bin/apache2-foreground
-
-echo "[hit enter key to exit] or run 'docker stop <container>'"
-read
-
-terminate()
+rm -f /var/run/apache2/apache2.pid
+exec apache2 -DFOREGROUND
