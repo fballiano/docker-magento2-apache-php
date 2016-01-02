2d94436bb3b519c7fea1f6c5dfc29aae264d1612
Fabrizio Balliano <fabrizio@fabrizioballiano.com>
2016-01-02 11:18:25 +0000

    some renamings

--- unregister-host-on-redis.php
+++ unregister-host-on-redis.php
@@ -3,5 +3,5 @@
 $host= gethostname();
 $ip = gethostbyname($host);
 require "/credis.php";
-$redis = new Credis_Client("redis_clusterdata");
+$redis = new Credis_Client("clusterdata");
 $redis->hDel("fb_apache_containers", $ip);
